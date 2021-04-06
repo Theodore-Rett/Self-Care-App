dd1a0b7748ab6554822fa4dacd1331a3916834c1
TeaganRett <teagan.rett@gmail.com>
2021-04-06 07:59:04 -0500

    Initial Commit

---
+++ Self Care App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Self Care App
+//
+//  Created by Tiger Coder on 4/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
