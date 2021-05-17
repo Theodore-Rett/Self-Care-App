ab5cdd11d7e8e43a5677181e8319f08c87eb77b6
TeaganRett <teagan.rett@gmail.com>
2021-05-17 08:15:57 -0500

    saved schedule

--- Self Care App/ScheduleViewController.swift
+++ Self Care App/ScheduleViewController.swift
@@ -16,9 +16,7 @@ class ScheduleViewController: UIViewController, UITableViewDelegate, UITableView
     
     //var schedule : [String : Date] = [:]
     
-    var schedule : [(event : String, time : Date)] = []
-    
-    
+    var schedule = [EventMaker]()
     
     let dateFormater = DateFormatter()
     
@@ -32,74 +30,97 @@ class ScheduleViewController: UIViewController, UITableViewDelegate, UITableView
         
         dateFormater.timeStyle = .short
         
-        if let list = defaults.object(forKey: "schedule"){
-            schedule = list as! [(event : String, time : Date)]
+        if let data = UserDefaults.standard.data(forKey: "savedEvents") {
+            do {
+                // Create JSON Decoder
+                let decoder = JSONDecoder()
+                
+                // Decode Note
+                schedule = try decoder.decode([EventMaker].self, from: data)
+                
+            } catch {
+                print("Unable to Decode Notes (\(error))")
+            }
         }
         
-       // if let list = defults.object(forKey: "save"){
-       //     schedule = list as! [EventMaker]
-       // }
+        //        if let list = defaults.object(forKey: "schedule"){
+        //            //schedule = list as! [(event : String, time : Date)]
+        //
+        //        }
+        
+        // if let list = defults.object(forKey: "save"){
+        //     schedule = list as! [EventMaker]
+        // }
         // Do any additional setup after loading the view.
     }
     
     @IBAction func addButton(_ sender: UIButton) {
         if(nameEnter.text != nil){
-            schedule.append((event: nameEnter.text!, time: timeInput.date))
+            //schedule.append((event: nameEnter.text!, time: timeInput.date))
+            schedule.append(EventMaker(inName: nameEnter.text!, inTime: timeInput.date))
             listDisplay.reloadData()
         }
     }
     
     @IBAction func saveButton(_ sender: UIButton) {
-        defaults.set(schedule, forKey: "schedule")
+        //defaults.set(schedule, forKey: "schedule")
+        do {
+            // Create JSON Encoder
+            let encoder = JSONEncoder()
+ 
+            // Encode Note
+            let data = try encoder.encode(schedule)
+            
+            // Write/Set Data
+            UserDefaults.standard.set(data, forKey: "savedEvents")
+        } catch {
+            print("Unable to Encode Array of Notes (\(error))")
+        }
     }
     
+    //    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    //        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell")!
+    //
+    //        //cell.textLabel?.text = (schedule[indexPath.row].String)
+    //
+    //        cell.textLabel?.text = schedule[indexPath.row].event
+    //
+    //        cell.detailTextLabel?.text = dateFormater.string(from: schedule[indexPath.row].time)
+    //
+    //        return cell
+    //    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return schedule.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell")!
+        let cell =  tableView.dequeueReusableCell(withIdentifier: "MyCell")!
         
-        //cell.textLabel?.text = (schedule[indexPath.row].String)
+        cell.textLabel?.text = ((schedule[indexPath.row].name))
         
-        cell.textLabel?.text = schedule[indexPath.row].event
-        
-        cell.detailTextLabel?.text = dateFormater.string(from: schedule[indexPath.row].time)
+        cell.detailTextLabel?.text = dateFormater.string(from: (schedule[indexPath.row].time))
         
         return cell
     }
     
-    
-    
-    
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let cell =  tableView.dequeueReusableCell(withIdentifier: "MyCell")!
-//
-//        cell.textLabel?.text = ((schedule[indexPath.row]) as! String)
-//
-//        cell.detailTextLabel?.text = dateFormater.string(from: (schedule[indexPath.row]) as! Date)
-//
-//        return cell
-//    }
-    
 //    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
 //        if(editingStyle == .delete){
 //
-//            schedule.remove(at: indexPath)
+//            schedule.remove(at: IndexPath)
 //
 //            tableView.deleteRows(at: [indexPath], with: .fade)
 //        }
 //    }
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
